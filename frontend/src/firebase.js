7395d86871e98fa4e6e0d688ca3851a86bce1e3b
Nachiket <nachiket.moghe@somaiya.edu>
2021-08-26 17:17:15 +0530

    Test commit

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,3 +6,10 @@ const firebaseConfig = {
     messagingSenderId: "916837686515",
     appId: "1:916837686515:web:15df79ce90184ef3c92b86"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+export default db;
+export {auth, provider}
